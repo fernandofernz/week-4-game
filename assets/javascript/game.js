eb84201eeeefe8ce2b028504840158a5fc6c292b
Ferny Dany <fernydany@fernys-mbp.attlocal.net>
2017-11-30 21:21:17 -0800

    Homework4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,103 +1,96 @@
 //ready method
-$(function(){
-// generate number to guess 19-120
+$(function () {
+    // generate number to guess 19-120
 
-var randomNumberToGuess = Math.floor(Math.random () * 101 + 19);
-console.log(randomNumberToGuess);
+    var randomNumberToGuess = Math.floor(Math.random() * 101 + 19);
+    console.log(randomNumberToGuess);
 
-// Display randomNumberToGuess
-$("#numberToGuess").text("Number to Guess :" + randomNumberToGuess);
-
-//variable declartation 
-var wins = 0;
-var losses = 0;
-var totalScore = 0;
-//diamond random number assigned 1-12
-var redDiamond = Math.floor(Math.random()* 11 + 1);
-var greenDiamond = Math.floor(Math.random()* 11 + 1);
-var blueDiamond = Math.floor(Math.random()* 11 + 1);
-var yellowDiamond = Math.floor(Math.random()* 11 + 1);
-
-console.log(redDiamond);
-console.log(greenDiamond);
-console.log(blueDiamond);
-console.log(yellowDiamond);
-
-
-resetGame = function() {
-
-  
-    totalScore = 0;
-    randomNumberToGuess = Math.floor(Math.random () * 101 + 19);
+    // Display randomNumberToGuess
     $("#numberToGuess").text("Number to Guess :" + randomNumberToGuess);
-    redDiamond = Math.floor(Math.random()* 11 + 1);
-    greenDiamond = Math.floor(Math.random()* 11 + 1);
-    blueDiamond = Math.floor(Math.random()* 11 + 1);
-    yellowDiamond = Math.floor(Math.random()* 11 + 1);
-}
+    $("#totalScore").text("Your total score is : ( Press a Diamond to begin to add)");
+    //variable declartation 
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    //diamond random number assigned 1-12
+    var redDiamond = Math.floor(Math.random() * 11 + 1);
+    var greenDiamond = Math.floor(Math.random() * 11 + 1);
+    var blueDiamond = Math.floor(Math.random() * 11 + 1);
+    var yellowDiamond = Math.floor(Math.random() * 11 + 1);
 
+    console.log(redDiamond);
+    console.log(greenDiamond);
+    console.log(blueDiamond);
+    console.log(yellowDiamond);
 
-$("#redDiamond").on('click', function() {
-    totalScore = totalScore + redDiamond;
-    $("#totalScore").text(totalScore);
-    console.log(totalScore);
-    if ( totalScore == randomNumberToGuess){
-        wins++;
-        $("#wins").text(wins);
-        resetGame();
-    }else if ( totalScore > randomNumberToGuess ){
-        losses++;
-        $("#losses").text(losses);
-        resetGame();
+    // reset game function
+    resetGame = function () {
+        totalScore = 0;
+        randomNumberToGuess = Math.floor(Math.random() * 101 + 19);
+        $("#numberToGuess").text("Number to Guess :" + randomNumberToGuess);
+        redDiamond = Math.floor(Math.random() * 11 + 1);
+        greenDiamond = Math.floor(Math.random() * 11 + 1);
+        blueDiamond = Math.floor(Math.random() * 11 + 1);
+        yellowDiamond = Math.floor(Math.random() * 11 + 1);
     }
-});
-$("#greenDiamond").on('click', function() {
-    totalScore = totalScore + greenDiamond;
-    $("#totalScore").text(totalScore);
-    console.log(totalScore);
-    if ( totalScore == randomNumberToGuess){
-        wins++;
-        $("#wins").text(wins);
-        resetGame();
-    }else if ( totalScore > randomNumberToGuess ){
-        losses++;
-        $("#losses").text(losses);
-        resetGame();
-    }
-});
-$("#blueDiamond").on('click', function() {
-    totalScore = totalScore + blueDiamond;
-    $("#totalScore").text(totalScore);
-    console.log(totalScore);
-    if ( totalScore == randomNumberToGuess){
-        wins++;
-        $("#wins").text(wins);
-        resetGame();
-    }else if ( totalScore > randomNumberToGuess ){
-        losses++;
-        $("#losses").text(losses);
-        resetGame();
-    }
-});
-$("#yellowDiamond").on('click', function() {
-    totalScore = totalScore + yellowDiamond;
-    $("#totalScore").text(totalScore);
-    console.log(totalScore);
-    if ( totalScore == randomNumberToGuess){
-        wins++;
-        $("#wins").text(wins);
-        resetGame();
-    }else if ( totalScore > randomNumberToGuess ){
-        losses++;
-        $("#losses").text(losses);
-        resetGame();
-    }
-});
 
+    // query selector with event listeners on each of the html image tags
+    $("#redDiamond").on('click', function () {
+        totalScore = totalScore + redDiamond;
+        $("#totalScore").text("Your total score is :" + totalScore);
+        console.log(totalScore);
 
-
-
-
-
+        if (totalScore == randomNumberToGuess) {
+            wins++;
+            $("#wins").text("Wins : " + wins);
+            resetGame();
+        } else if (totalScore > randomNumberToGuess) {
+            losses++;
+            $("#losses").text("Losses : " + losses);
+            resetGame();
+        }
+    });
+    $("#greenDiamond").on('click', function () {
+        totalScore = totalScore + greenDiamond;
+        $("#totalScore").text("Your total score is :" + totalScore);
+        console.log(totalScore);
+        if (totalScore == randomNumberToGuess) {
+            wins++;
+            $("#wins").text("Wins : " + wins);
+            resetGame();
+        } else if (totalScore > randomNumberToGuess) {
+            losses++;
+            $("#losses").text("Losses : " + losses);
+            resetGame();
+        }
+    });
+    $("#blueDiamond").on('click', function () {
+        totalScore = totalScore + blueDiamond;
+        $("#totalScore").text("Your total score is :" + totalScore);
+        console.log(totalScore);
+        if (totalScore == randomNumberToGuess) {
+            wins++;
+            $("#wins").text("Wins : " + wins);
+            resetGame();
+        } else if (totalScore > randomNumberToGuess) {
+            losses++;
+            $("#losses").text("Losses : " + losses);
+            resetGame();
+        }
+    });
+    $("#yellowDiamond").on('click', function () {
+        totalScore = totalScore + yellowDiamond;
+        $("#totalScore").text("Your total score is :" + totalScore);
+        console.log(totalScore);
+        if (totalScore == randomNumberToGuess) {
+            wins++;
+            $("#wins").text("Wins : " + wins);
+            resetGame();
+        } else if (totalScore > randomNumberToGuess) {
+            losses++;
+            $("#losses").text("Losses : " + losses);
+            resetGame();
+        }
+    });
 });
 //closed ready method
